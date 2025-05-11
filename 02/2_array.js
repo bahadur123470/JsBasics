c77f2637e026e0bb6de86f88f2d4cbcdc04c8d2f
Bahadur Ali <bahadurali123470@gmail.com>
2025-05-11 14:02:41 +0500

    Add JavaScript basics for array methods and manipulation examples

---
+++ 02/2_array.js
@@ -0,0 +1,31 @@
+const marvel = [" Iron Man", "Captain America", "Thor",];
+const dc = ["Superman", "Batman", "The Flash", "Green Lantern"];
+
+//  Not a good practice to use
+// marvel.push(dc)
+// console.log(marvel)
+
+//  Often used but not recommended
+const hero = marvel.concat(dc)
+// console.log(hero)
+
+// Recommended way 
+const heroes = [...marvel, ...dc]
+// console.log(heroes)
+
+
+// const numArr = [1,2,3,[4,5,[6,7,8], [9,10,[11,12,3,5,9]]], 13, 14, 15];
+// console.log(numArr)
+// const flatArr = numArr.flat(Infinity)
+// console.log(flatArr)
+
+
+console.log(Array.isArray("bahadur"))
+console.log(Array.from("bahadur"))
+console.log(Array.from({name: "BAHADUR"})) // interesting 
+
+let value1 = 500 
+let value2 = 1200
+let value3 = 3000
+
+console.log(Array.of(value1, value2, value3)) 
