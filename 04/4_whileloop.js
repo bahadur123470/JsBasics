2cc21219e984b466483964b6a02e75928ceba670
Bahadur Ali <bahadurali123470@gmail.com>
2025-05-14 19:39:47 +0500

    Add examples of for loops, while loops, and do-while loops with comments

---
+++ 04/4_whileloop.js
@@ -0,0 +1,38 @@
+// let  x = 0 
+// while (x <= 10) {
+//     console.log(`Power of x is ${x}`)
+//     x = x + 2
+//     // x++
+// }
+
+let index = 0
+while (index <= 20){
+    // console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+let avengerArr = [ " Iron Man", "Captain America", "Thor", "Black Widow", "Hulk"]
+let arr = 1
+while ( arr <avengerArr.length ){
+    // console.log(`avenger ${arr} is ${avengerArr[arr]}`)
+    arr = arr + 1
+}
+
+let count = 0
+while ( count <= 5){
+    // console.log(`Countdown ${count}`)
+    count++;
+}
+
+let even = 2
+while( even <= 20){
+    // console.log(`${even} is a even number`)
+    // even = even + 2
+    even += 2
+}
+
+let six = 6
+while (six <= 60){
+    console.log(` it's a six! and six run added in a scorecard  PAK-${six}`)
+    six += 6
+}
