55272362ea2b8d36799107590969423b7949ba06
Bahadur Ali <bahadurali123470@gmail.com>
2025-05-14 19:40:29 +0500

    Uncomment examples of do-while loops for user login, dice rolling, and coin toss

--- 04/5_Doloop.js
+++ 04/5_Doloop.js
@@ -1,43 +1,43 @@
-// let chaseTarget = 0
-// do {
-//     // console.log(`match start ${chaseTarget}`);
-//     chaseTarget++
-// } while ( chaseTarget <= 49);
+let chaseTarget = 0
+do {
+    // console.log(`match start ${chaseTarget}`);
+    chaseTarget++
+} while ( chaseTarget <= 49);
 
-// let userLoggedIn = false
-// let attempt = 0
-// do {
-//     console.log(`User Attempting to Login`);
-//     attempt++
+let userLoggedIn = false
+let attempt = 0
+do {
+    console.log(`User Attempting to Login`);
+    attempt++
 
-//     if (attempt === 3){
-//         userLoggedIn = true;
-//         console.log(`User has logged in successful after ${attempt} attempts`);
-//     }
-// } while (!userLoggedIn);
-
-
-// let diceRoll = 3
-// let guess;
-// let roll = 0 
-// do {
-//     guess = Math.floor(Math.random() * 10)
-//     console.log(`You rolled a ${guess}`);
-//     if(guess == diceRoll){
-//         console.log(`You rolled a ${diceRoll} and guessed it!`);
-//     }
-//     else {
-//         console.log("Wrong guess try again")
-//     }
-// } while (guess !== diceRoll);
+    if (attempt === 3){
+        userLoggedIn = true;
+        console.log(`User has logged in successful after ${attempt} attempts`);
+    }
+} while (!userLoggedIn);
 
 
-// let toss;
-// do {
-//     toss = Math.random() < 0.5 ? "Tail" : "Head";
-//     console.log(`You got ${toss}`)
-// } while (toss !== "Head");
-// console.log(`----Finally you got a ${toss}-----`);  
+let diceRoll = 3
+let guess;
+let roll = 0 
+do {
+    guess = Math.floor(Math.random() * 10)
+    console.log(`You rolled a ${guess}`);
+    if(guess == diceRoll){
+        console.log(`You rolled a ${diceRoll} and guessed it!`);
+    }
+    else {
+        console.log("Wrong guess try again")
+    }
+} while (guess !== diceRoll);
+
+
+let toss;
+do {
+    toss = Math.random() < 0.5 ? "Tail" : "Head";
+    console.log(`You got ${toss}`)
+} while (toss !== "Head");
+console.log(`----Finally you got a ${toss}-----`);  
 
 
 let carCharge = 0;
